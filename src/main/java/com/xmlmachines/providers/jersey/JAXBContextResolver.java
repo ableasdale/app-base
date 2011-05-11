5872208c266fcb5e87ab580a68c33ddd10ca7eec
Alex Bleasdale <ableasdale@marklogic.com>
2011-05-11 17:24:31 +0100

    package scanner for JAXBContext now works properly

--- src/main/java/com/xmlmachines/providers/jersey/JAXBContextResolver.java
+++ src/main/java/com/xmlmachines/providers/jersey/JAXBContextResolver.java
@@ -1,18 +1,20 @@
 package com.xmlmachines.providers.jersey;
 
-import java.util.Arrays;
+import java.text.MessageFormat;
 import java.util.HashSet;
 import java.util.Set;
 
 import javax.ws.rs.ext.ContextResolver;
 import javax.ws.rs.ext.Provider;
 import javax.xml.bind.JAXBContext;
+import javax.xml.bind.annotation.XmlRootElement;
 
 import org.apache.log4j.Logger;
 
 import com.sun.jersey.api.json.JSONConfiguration;
 import com.sun.jersey.api.json.JSONJAXBContext;
-import com.xmlmachines.beans.jaxb.ExampleBean;
+import com.sun.jersey.core.spi.scanning.PackageNamesScanner;
+import com.sun.jersey.spi.scanning.AnnotationScannerListener;
 
 @Provider
 public final class JAXBContextResolver implements ContextResolver<JAXBContext> {
@@ -21,21 +23,29 @@ public final class JAXBContextResolver implements ContextResolver<JAXBContext> {
 			.getLogger(JAXBContextResolver.class);
 
 	private final JAXBContext context;
-
 	private final Set<Class<?>> types;
-
-	private final Class<?>[] cTypes = { ExampleBean.class };
+	private final Class<?>[] cTypes;
 
 	public JAXBContextResolver() throws Exception {
 		LOG.info("Setting up JAXB Context resolver..");
-		this.types = new HashSet<Class<?>>(Arrays.asList(cTypes));
+
+		PackageNamesScanner pns = new PackageNamesScanner(
+				new String[] { "com.xmlmachines.beans" });
+		@SuppressWarnings("unchecked")
+		AnnotationScannerListener asl = new AnnotationScannerListener(
+				XmlRootElement.class); // , XmlType.class
+		pns.scan(asl);
+		this.types = new HashSet<Class<?>>(asl.getAnnotatedClasses());
+
+		cTypes = new Class[this.types.size()];
 		this.context = new JSONJAXBContext(JSONConfiguration.natural().build(),
-				cTypes);
+				this.types.toArray(cTypes));
 	}
 
 	@Override
 	public JAXBContext getContext(Class<?> objectType) {
-		LOG.info("Do we have a context? " + types.contains(objectType));
+		LOG.info(MessageFormat.format("Do we have a context? {0}",
+				types.contains(objectType)));
 		return (types.contains(objectType)) ? context : null;
 	}
 }
