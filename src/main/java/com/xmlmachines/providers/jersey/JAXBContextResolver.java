83eb3fab751cc7b9833d62025b25b1b36664d0f2
Alex Bleasdale <ableasdale@marklogic.com>
2011-05-11 11:03:40 +0100

    Working but with hardcoded list of classes for JAXB context

--- src/main/java/com/xmlmachines/providers/jersey/JAXBContextResolver.java
+++ src/main/java/com/xmlmachines/providers/jersey/JAXBContextResolver.java
@@ -1,6 +1,5 @@
 package com.xmlmachines.providers.jersey;
 
-import java.text.MessageFormat;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
@@ -8,14 +7,12 @@ import java.util.Set;
 import javax.ws.rs.ext.ContextResolver;
 import javax.ws.rs.ext.Provider;
 import javax.xml.bind.JAXBContext;
-import javax.xml.bind.annotation.XmlRootElement;
-import javax.xml.bind.annotation.XmlType;
 
 import org.apache.log4j.Logger;
 
 import com.sun.jersey.api.json.JSONConfiguration;
 import com.sun.jersey.api.json.JSONJAXBContext;
-import com.sun.jersey.server.impl.container.config.AnnotatedClassScanner;
+import com.xmlmachines.beans.jaxb.ExampleBean;
 
 @Provider
 public final class JAXBContextResolver implements ContextResolver<JAXBContext> {
@@ -25,22 +22,20 @@ public final class JAXBContextResolver implements ContextResolver<JAXBContext> {
 
 	private final JAXBContext context;
 
-	private final HashSet<Class<?>> types; // = new HashSet<Class<?>>();
+	private final Set<Class<?>> types;
+
+	private final Class<?>[] cTypes = { ExampleBean.class };
 
 	public JAXBContextResolver() throws Exception {
-		LOG.info("Setting up the JAXBContextResolver");
-		AnnotatedClassScanner classScanner = new AnnotatedClassScanner(
-				XmlRootElement.class, XmlType.class);
-		Set<Class<?>> cTypes = classScanner
-				.scan(new String[] { "com.xmlmachines.beans" });
-		this.types = new HashSet(Arrays.asList(cTypes));
+		LOG.info("Setting up JAXB Context resolver..");
+		this.types = new HashSet<Class<?>>(Arrays.asList(cTypes));
 		this.context = new JSONJAXBContext(JSONConfiguration.natural().build(),
-				cTypes.toArray(new Class[cTypes.size()]));
+				cTypes);
 	}
 
 	@Override
 	public JAXBContext getContext(Class<?> objectType) {
-		LOG.info(MessageFormat.format("Getting context for: {0}", objectType));
+		LOG.info("Do we have a context? " + types.contains(objectType));
 		return (types.contains(objectType)) ? context : null;
 	}
 }
